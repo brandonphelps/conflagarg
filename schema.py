4fd22581412d2c44ee279dc255b2ccc966a1d1ac
Micheal Wall <mmdj4u@gmail.com>
2017-03-21 22:34:29 -0500

    schema.py: Bug fixes and PEP8 implementations.

--- schema.py
+++ schema.py
@@ -1,31 +1,38 @@
 #!/usr/bin/env python3
 
+"""
+conflagarg schema module
+"""
+
 from collections import OrderedDict
 
-class Schema(self):
+class Schema():
+    """
+    A class for defining a conflagarg schema.
+    """
     def __init__(self):
-        """
-        A class for defining a conflagarg schema.
-        """
         self._params = OrderedDict() #preserve the user-defined order
-        
+
     def sections(self):
         """
         Returns the sections as part of this schema.
         """
         return list(self._params.keys())
-    
+
     def add_section(self, section):
+        """
+        Add a section to the schema.
+        """
         if section in self._params:
             raise ValueError("Section is already part of schema.")
         self._params[section] = None
-        
+
     def params(self):
         """
         Returns the parameters currently part of this schema.
         """
         return dict(self._params)
-    
+
     def add_param(self, key, value=None, section=None, required=False, check=None):
         """
         Add a parameter definition to the schema.
@@ -34,10 +41,9 @@ class Schema(self):
             raise ValueError("check must be a function.")
         else:
             check = lambda: True
-        
-        s = section
-        if not len(self._params) and s is None:
+
+        if not len(self._params) and section is None:
             #There are no sections. We should not require the dev to define
             #any.
-            s = "Global"
-        self._params[s] = (key,  value, required, check)
+            section = "Default"
+        self._params[section] = (key, value, required, check)
